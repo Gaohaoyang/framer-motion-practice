e3d70b70fa943cb573f5a9d4e303a56eb288377d
chuanshi <ghy118432@alibaba-inc.com>
2021-01-19 09:07:38 +0800

    s

--- src/components/AnimationElements.jsx
+++ src/components/AnimationElements.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 function AnimationElements() {
-  return <div>AnimationElements</div>
+  return (
+    <div>
+      <motion.h2
+        initial={false}
+        // initial={{
+        //   y: -20,
+        // }}
+        animate={{
+          x: 50,
+          y: 10,
+          scale: 1.2,
+        }}
+      >
+        AnimationElements
+      </motion.h2>
+    </div>
+  )
 }
 
 export default AnimationElements
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
-// import { motion } from 'framer-motion'
-import './Home.css'
 import { Link } from 'react-router-dom'
 
 function intro() {
   return (
-    <div>
+    <>
       <h1>hello framer-motion</h1>
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
         <li>
           <Link to="/AnimationElements">AnimationElements</Link>
         </li>
@@ -18,9 +13,7 @@ function intro() {
           <Link to="/Variants">Variants</Link>
         </li>
       </ul>
-      {/* <motion.div className="motion-intro" animate={{ scale: 1.5 }} /> */}
-
-    </div>
+    </>
   )
 }
 
